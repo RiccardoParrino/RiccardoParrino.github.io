304c1c1ee4f5d4d97b7138dd8d617fbbc6ef12f1
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-27 17:14:31 +0200

    update routes

--- MyBlog/src/app/app.routes.ts
+++ MyBlog/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { TutorialsComponent } from './tutorials/tutorials.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'Home',
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: 'About',
+        component: AboutComponent,
+        title: 'About Page'
+    },
+    {
+        path: 'Contact',
+        component: ContactComponent,
+        title: 'Contact Page'
+    },
+    {
+        path: 'Tutorials',
+        component: TutorialsComponent,
+        title: 'Tutorials page'
+    }
+
+];
