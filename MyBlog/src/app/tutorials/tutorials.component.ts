ea19c24f889c3cb0287cadeecef6f441c9f490f1
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-29 18:58:41 +0200

    fix error

--- MyBlog/src/app/tutorials/tutorials.component.ts
+++ MyBlog/src/app/tutorials/tutorials.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import { RouterLink } from "../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 
 @Component({
   selector: 'app-tutorials',
-  imports: [MatCardModule, MatButtonModule, RouterLink],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './tutorials.component.html',
   styleUrl: './tutorials.component.css'
 })
