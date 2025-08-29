8c1f2b29478e6def203aa854fc1d860a5de0eb14
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-29 18:43:19 +0200

    tutorials page

--- MyBlog/src/app/tutorials/tutorials.component.ts
+++ MyBlog/src/app/tutorials/tutorials.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-tutorials',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './tutorials.component.html',
   styleUrl: './tutorials.component.css'
 })
