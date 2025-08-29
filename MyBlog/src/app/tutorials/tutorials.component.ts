6ed0f03b8044104133c6d1c86680031038683d35
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-29 18:57:35 +0200

    tutorial component updated

--- MyBlog/src/app/tutorials/tutorials.component.ts
+++ MyBlog/src/app/tutorials/tutorials.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { RouterLink } from "../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 
 @Component({
   selector: 'app-tutorials',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, RouterLink],
   templateUrl: './tutorials.component.html',
   styleUrl: './tutorials.component.css'
 })
