2df2f841ed359f985e68a58f18ee64093e10612c
RiccardoParrino <riccardoparrino1@gmail.com>
2025-08-30 17:49:21 +0200

    basic post routing

--- MyBlog/src/app/post/post.component.ts
+++ MyBlog/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -6,6 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+
+  content = '';
+
+  posts = [
+    { "id": "basic-testing-in-angular", "text": "Basic testing in Angular" },
+    { "id": "advanced-component-in-angular", "text": "Advanced component in angular" },
+    { "id": "http-requests-in-angular", "text": "Http requests in Angular"},
+    { "id": "forms-in-angular", "text": "Forms in Angular"}
+  ];
+
+  constructor (private route: ActivatedRoute) {}
+
+  ngOnInit () {
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.content = this.findContentById(id);
+  }
+
+  findContentById(id:string) : string {
+    for (let post of this.posts) {
+      if (post.id == id) {
+        return post.text;
+      }
+    }
+    return "";
+  }
 
 }
